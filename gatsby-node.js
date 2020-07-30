895993e069cd9dcdd9377919b613409c355ce8c1
pcollinsTech <phil@pcollins.tech>
2020-07-30 12:20:10 +0100

    Strapi remove

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,43 @@
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const result = await graphql(
-    `
-      {
-        posts: allStrapiPost {
-          edges {
-            node {
-              strapiId
-            }
-          }
-        }
-      }
-    `
-  );
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   const result = await graphql(
+//     `
+//       {
+//         posts: allStrapiPost {
+//           edges {
+//             node {
+//               strapiId
+//             }
+//           }
+//         }
+//       }
+//     `
+//   );
 
-  if (result.errors) {
-    throw result.errors;
-  }
+//   if (result.errors) {
+//     throw result.errors;
+//   }
 
-  // Create blog posts pages.
-  const posts = result.data.posts.edges;
+//   // Create blog posts pages.
+//   const posts = result.data.posts.edges;
 
-  posts.forEach((post, index) => {
-    createPage({
-      path: `/blog/${post.node.strapiId}`,
-      component: require.resolve("./src/templates/post.js"),
-      context: {
-        id: post.node.strapiId,
-      },
-    });
-  });
+//   posts.forEach((post, index) => {
+//     createPage({
+//       path: `/blog/${post.node.strapiId}`,
+//       component: require.resolve("./src/templates/post.js"),
+//       context: {
+//         id: post.node.strapiId,
+//       },
+//     });
+//   });
 
-  //   projects.forEach((project, index) => {
-  //     createPage({
-  //       path: `/project/${project.node.strapiId}`,
-  //       component: require.resolve("./src/templates/project.js"),
-  //       context: {
-  //         id: project.node.strapiId,
-  //       },
-  //     });
-  //   });
-};
+//   //   projects.forEach((project, index) => {
+//   //     createPage({
+//   //       path: `/project/${project.node.strapiId}`,
+//   //       component: require.resolve("./src/templates/project.js"),
+//   //       context: {
+//   //         id: project.node.strapiId,
+//   //       },
+//   //     });
+//   //   });
+// };
