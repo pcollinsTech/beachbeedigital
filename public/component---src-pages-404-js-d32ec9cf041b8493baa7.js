bf3ea763c7a730d073ede8a612512ff93d71c2a8
pcollinsTech <phil@pcollins.tech>
2021-01-05 12:47:34 +0000

    hi

---
+++ public/component---src-pages-404-js-d32ec9cf041b8493baa7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),s=a("x6fP");t.default=function(){return l.a.createElement(s.a,null,l.a.createElement("div",{id:"main",className:"alt"},l.a.createElement("section",{id:"one"},l.a.createElement("div",{className:"inner"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d32ec9cf041b8493baa7.js.map
